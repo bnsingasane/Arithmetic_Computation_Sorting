2ab9b196ac6fed30ba99c4f9fd37191a7789573e
bhagyashree <bnsingasane@gmail.com>
2020-07-18 02:35:32 +0530

    Refactor:sort the result in ascending order

--- arithmetic_sorting.sh
+++ arithmetic_sorting.sh
@@ -54,3 +54,23 @@ do
         echo  ${arr[i]}
 done
 
+#sort the result in descending order
+temp=0
+for((i=0;i<4;i++))
+do
+        for((j=0;j<4-i-1;j++))
+        do
+                if [ ${arr[j]} -lt ${arr[$((j+1))]} ]
+                then
+                        temp=${arr[j]}
+                        arr[$j]=${arr[$((j+1))]}
+                        arr[$((j+1))]=$temp
+                fi
+        done
+done
+echo "Descending order is:"
+
+for((i=0;i<4;i++))
+do
+        echo  ${arr[i]}
+done
