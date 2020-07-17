69a0e2c296b4f38b8728d90101ba600024a25900
bhagyashree <bnsingasane@gmail.com>
2020-07-18 01:38:03 +0530

    Added:taking three inputs from user

--- arithmetic_sorting.sh
+++ arithmetic_sorting.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 
+#Taking input
+echo "Enter the value of a:"
+read a
+echo "Enter the value of b:"
+read b
+echo "Enter the value of c:"
+read c
+
+#compute a+b*c
+op1=$(($a+$b*$c))
